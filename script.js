78b2f6148dadae7446b9a9fd1f36a83fa16b5d24
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-10-06 01:23:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    const obj = {
+        0: ['M', 1000],
+        1: ['D', 500],
+        2: ['C', 100],
+        3: ['L', 50],
+        4: ['X', 10],
+        5: ['V', 5],
+        6: ['I', 1]
     };
 
-  //your code here
+    let romanNumeral = '';
 
+    for (let i = 0; i < 7; i++) {
+        const symbol = obj[i][0];
+        const value = obj[i][1];
+
+        // Calculate the number of times the current symbol should appear
+        const count = Math.floor(num / value);
+
+        // Append the symbol to the result
+        romanNumeral += symbol.repeat(count);
+
+        // Update the remaining number
+        num %= value;
+    }
+
+    return romanNumeral;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// Example usage:
+console.log(convertToRoman(36)); // Output: "XXXVI"
